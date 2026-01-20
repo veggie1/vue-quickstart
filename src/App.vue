e0a64d9eacfddaf2b069824ed9e6dcc80bdff682
Ben Baacke <ben.baacke@gmail.com>
2026-01-20 14:25:35 -0500

    Add dynamic list and prop binding

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <script lang="ts">
+import { nanoid } from 'nanoid'
 import ToDoItem from './components/ToDoItem.vue'
 
 export default {
   name: 'app',
   components: {
     ToDoItem,
+  },
+  data() {
+    return {
+      ToDoItems: [
+        { id: `todo-${nanoid()}`, label: 'Learn Vue', done: false },
+        {
+          id: `todo-${nanoid()}`,
+          label: 'Create a Vue project with the CLI',
+          done: true,
+        },
+        { id: `todo-${nanoid()}`, label: 'Have fun', done: true },
+        {
+          id: `todo-${nanoid()}`,
+          label: 'Create a to-do list',
+          done: false,
+        },
+      ],
+    }
   },
 }
 </script>
@@ -13,8 +32,8 @@ export default {
   <div id="app">
     <h1>To-Do List</h1>
     <ul>
-      <li>
-        <to-do-item label="My ToDo Item" :done="true"></to-do-item>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
       </li>
     </ul>
   </div>
--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -9,11 +9,11 @@ export default {
   props: {
     label: { required: true, type: String },
     done: { default: false, type: Boolean },
+    id: { required: true, type: String },
   },
   data() {
     return {
       isDone: this.done,
-      id: `todo-${crypto.randomUUID()}`,
     }
   },
 }
