5ebe8beb20cdb6fc26306280e2cde8bc8b3356b3
Ben Baacke <ben.baacke@gmail.com>
2026-01-21 09:38:39 -0500

    Implement input with add button

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script lang="ts">
 import { nanoid } from 'nanoid'
 import ToDoItem from './components/ToDoItem.vue'
+import ToDoForm from './components/ToDoForm.vue'
 
 export default {
   name: 'app',
   components: {
     ToDoItem,
+    ToDoForm,
   },
   data() {
     return {
@@ -25,12 +27,22 @@ export default {
       ],
     }
   },
+  methods: {
+    addToDo(toDoLabel: string) {
+      this.ToDoItems.push({
+        id: `todo-${nanoid()}`,
+        label: toDoLabel,
+        done: false,
+      })
+    },
+  },
 }
 </script>
 
 <template>
   <div id="app">
-    <h1>To-Do List</h1>
+    <h1>My To-Do List</h1>
+    <to-do-form @todo-added="addToDo"></to-do-form>
     <ul>
       <li v-for="item in ToDoItems" :key="item.id">
         <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <label for="new-todo-input"> What needs to be done? </label>
+    <input
+      type="text"
+      id="new-todo-input"
+      name="new-todo"
+      autocomplete="off"
+      v-model.lazy.trim="label"
+    />
+    <button type="submit">Add</button>
+  </form>
+</template>
+
+<script lang="ts">
+export default {
+  methods: {
+    onSubmit() {
+      if (!!this.label) {
+        this.$emit('todo-added', this.label)
+        this.label = ''
+      }
+    },
+  },
+  data() {
+    return {
+      label: '',
+    }
+  },
+}
+</script>
